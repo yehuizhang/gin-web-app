cf91ad175c0739129444382df6b818095b01a60f
Yehui Zhang <Yehuizhang@outlook.com>
2020-10-22 03:02:40 -0700

    Endpoint is taking params

--- main.go
+++ main.go
@@ -1,14 +1,35 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 
-	r.Run()
+	r.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.JSON(200, gin.H{
+			"message": name,
+		})
+	})
+
+	r.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		c.JSON(http.StatusOK, gin.H{
+			"name":   name,
+			"action": action,
+		})
+	})
+
+	r.Run(":3001")
 }
